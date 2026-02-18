e8251c3968ae10d74706c651b87d335aebb8667b
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2026-02-18 13:38:09 +0100

    feat: use lualine instead of mini.statusline

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,19 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      icons_enabled = vim.g.have_nerd_font,
+      component_separators = { left = '|', right = '|' },
+      section_separators = { left = '', right = '' },
+    },
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
+}
